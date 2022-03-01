6f49240ffba3ae06a026d324460e6a4928eddd5a
patelpooja1321 <apatel4@aum.edu>
2022-03-01 16:23:25 -0600

    ch

--- userinput.go
+++ userinput.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	msg := "Hello"
+	msg := "Practice of user input"
 	fmt.Println(msg)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter rating for Pizza:")
+
+	//comma or || error or
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for rating", input)
+
 }
