8949a04a56ee75cc45848ede5e4048f00d0c67c9
akash0813 <apatel4@aum.edu>
2022-03-01 12:11:08 -0600

    change1

---
+++ variables.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Pooja"
+	fmt.Println("My name is", name)
+	fmt.Printf("Variable data type is %T \n", name)
+	fmt.Printf("Variable Value is %s \n", name)
+}
