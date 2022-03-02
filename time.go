6cda38eb3b3c52f72fa7d8fecc8520f532eddbe2
patelpooja1321 <apatel4@aum.edu>
2022-03-02 14:49:32 -0600

    edit1

--- time.go
+++ time.go
@@ -10,6 +10,6 @@ func main() {
 	fmt.Println("Welcome to time Class")
 	currenttime := time.Now()
 	fmt.Println(currenttime)
-	fmt.Println("Time Formate:", currenttime.Format("02-01-2022 Monday"))
+	fmt.Println("Time Formate:", currenttime.Format("02-01-2022 Monday 15:04:05 PM"))
 
 }
