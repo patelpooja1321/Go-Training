1b4b997fcc49ac7023515e86bb9f5a5023d0ee9a
patelpooja1321 <apatel4@aum.edu>
2022-03-02 15:01:20 -0600

    edit2

--- time.go
+++ time.go
@@ -12,4 +12,8 @@ func main() {
 	fmt.Println(currenttime)
 	fmt.Println("Time Formate:", currenttime.Format("02-01-2022 Monday 15:04:05 PM"))
 
+	createdate := time.Date(2021, time.February, 4, 03, 15, 2, 0, time.UTC)
+	fmt.Println(createdate)
+	fmt.Println(createdate.Format("01-04-2022 Monday"))
+
 }
