03cb6bb5a1c665f9f5dca7c868675b91dc2f021e
patelpooja1321 <apatel4@aum.edu>
2022-03-11 16:44:44 -0600

    chh

--- addtwo.go
+++ addtwo.go
@@ -6,19 +6,26 @@ var arr1 = []int{2, 4, 3}
 var arr2 = []int{5, 6, 4}
 var sum []int
 var count int
+var c int
 
 func main() {
 	fmt.Println("Add two array")
-	l := len(arr1)
-	for i := 0; i < l; i++ {
-		fmt.Println(arr1[i], arr2[i])
+	fmt.Println("First Array:", arr1)
+	fmt.Println("Second Array:", arr2)
+	l := len(arr1) - 1
+	for i := l; i >= 0; i-- {
 		sum1 := arr1[i] + arr2[i]
-		c := sum1 / 10
+
 		if c == 1 {
 			sum1 = sum1 + 1
-			fmt.Println(sum)
+			sum = append(sum, sum1)
+			c = 0
+		} else {
+			c1 := sum1 % 10
+			sum = append(sum, c1)
+			c = sum1 / 10
 		}
-		sum = append(sum, sum1)
+
 	}
 	fmt.Println(sum)
 }
