4be64233d92fc269709ecf0fbe9d311f966f7a3d
patelpooja1321 <apatel4@aum.edu>
2022-03-14 17:15:00 -0500

    newch

--- method.go
+++ method.go
@@ -13,9 +13,15 @@ func main() {
 	std := person_details{"Meera", 12345, true, 12345678}
 	fmt.Println("Students Details:", std)
 	std.Student()
+	fmt.Println("Students Details:", std)
 
 }
 
 func (s person_details) Student() {
 	fmt.Println("Students Status:", s.Status)
+}
+
+func (s *person_details) New_phone() {
+	s.phone = 3456832
+	fmt.Println("Students Status:", s.phone)
 }
